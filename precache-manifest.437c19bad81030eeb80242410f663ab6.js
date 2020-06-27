c5eabb14acce879594ccb965a263451fd8841943
zhoudaxia233 <yootaoo@gmail.com>
2020-06-27 22:54:50 +0200

    Updates

---
+++ precache-manifest.437c19bad81030eeb80242410f663ab6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eff2a3466121c19b6ed2e6ba8b864e8a",
+    "url": "/geo/index.html"
+  },
+  {
+    "revision": "03bf494dd45cb6d105b0",
+    "url": "/geo/static/css/main.d05399b0.chunk.css"
+  },
+  {
+    "revision": "450b2a3ec528eab86091",
+    "url": "/geo/static/js/2.08c4ca8c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/geo/static/js/2.08c4ca8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03bf494dd45cb6d105b0",
+    "url": "/geo/static/js/main.1de4dbfb.chunk.js"
+  },
+  {
+    "revision": "2af5c962b039a1abf7e9",
+    "url": "/geo/static/js/runtime-main.cd739b78.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5b1acf37cd21e952e9deead38c048005.js"
+  "/geo/precache-manifest.437c19bad81030eeb80242410f663ab6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/geo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
